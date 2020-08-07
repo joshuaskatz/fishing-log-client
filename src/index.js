3af913d9fab61f917f14cdfeff8600179d919c97
Joshua <fujin95@gmail.com>
2020-08-07 14:59:51 -0400

    added setupProxy.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-	uri: process.env.REACT_APP_URI
+	uri: '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
